291739ab72ab28d612b0083361557a3357954076
Belingheri <Belingheri@duck.com>
2023-12-28 11:58:42 +0100

    game cards add github pages

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,9 @@ import "../scss/styles.scss";
 
 // Import all of Bootstrap's JS
 import * as bootstrap from "bootstrap";
+
+import socialBadgesHTML from "../html-sections/social-badges.js";
+
+document.querySelectorAll(".social-badges").forEach((el) => {
+  el.innerHTML = socialBadgesHTML;
+});
